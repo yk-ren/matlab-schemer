b17f244d1b3e8bbab3240476b36bfd19992af515
yk-ren <meykren@gmail.com>
2023-04-13 14:12:22 +0100

    Apr13

--- s_ykColorThemeSetting.m
+++ s_ykColorThemeSetting.m
@@ -4,14 +4,14 @@
 % lu: 230412
 
 %% addpath
-pth = 'D:\OneDrive - Imperial College London\Proj\IcProj\';
+pth = 'D:\OneDrive - Imperial College London\Proj\IcProj\SettingsRep\matlab-schemer';
 addpath(genpath(pth));
 
 %% Change the color theme
 
 addpath('./schemes');  
 
-schemer_import('darkmate.prf');
+schemer_import('darksteel.prf');
 % Options:
     % darkmate.prf 
     % darksteel.prf
@@ -20,11 +20,11 @@ rmpath('./schemes');
 
 %% Revert to the default color theme of MATLAB
 
-addpath('./schemes');  
-
-schemer_import('default.prf');
-
-rmpath('./schemes');
+% addpath('./schemes');  
+% 
+% schemer_import('default.prf');
+% 
+% rmpath('./schemes');
 
 %% rmpath
 rmpath(genpath(pth));
